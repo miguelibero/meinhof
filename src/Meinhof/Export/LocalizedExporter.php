574e7da41d2832536721fd85e9669b04a341b72d
Miguel Ibero <miguel.ibero.carreras@gmail.com>
2012-08-04 19:29:12 +0200

    fixed localized exporter bug

--- src/Meinhof/Export/LocalizedExporter.php
+++ src/Meinhof/Export/LocalizedExporter.php
@@ -2,6 +2,7 @@
 
 namespace Meinhof\Export;
 
+use Symfony\Component\Translation\TranslatorInterface;
 /**
  * Exports each element for a list of locales
  *
@@ -11,17 +12,20 @@ class LocalizedExporter implements ExporterInterface
 {
     protected $locales;
     protected $exporter;
+    protected $translator;
 
-    public function __construct(array $locales, ExporterInterface $exporter)
+    public function __construct(array $locales, ExporterInterface $exporter, TranslatorInterface $translator)
     {
         $this->locales = $locales;
         $this->exporter = $exporter;
+        $this->translator = $translator;
     }
 
     public function export($model, $template, array $parameters)
     {
         foreach ($this->locales as $locale) {
             $parameters['locale'] = $locale;
+            $this->translator->setLocale($locale);
             $this->exporter->export($model, $template, $parameters);
         }
     }  
