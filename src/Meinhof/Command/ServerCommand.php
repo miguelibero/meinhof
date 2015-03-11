af7a3aaf063a081e248fb14633ba38f724fe5232
Alberto Fernández <albertofem@gmail.com>
2015-03-11 23:53:05 +0100

    Added watch and server commands

--- src/Meinhof/Command/ServerCommand.php
+++ src/Meinhof/Command/ServerCommand.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(ticks = 1);
+
 namespace Meinhof\Command;
 
 use Meinhof\Meinhof;
@@ -7,13 +9,15 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\PhpExecutableFinder;
+use Symfony\Component\Process\ProcessBuilder;
 
 /**
  * Command that loads a webserver and listens to changes
  *
  * @author Miguel Ibero <miguel@ibero.me>
  */
-class ServerCommand extends MeinhofCommand
+class ServerCommand extends WatchCommand
 {
     /**
      * @{inheritDoc}
@@ -25,8 +29,8 @@ class ServerCommand extends MeinhofCommand
             ->setDescription('Starts a local server and watches for changes')
             ->setDefinition(array(
                  new InputArgument('dir', InputArgument::OPTIONAL, 'base directory of the site configuration', '.'),
-                 new InputOption('port', 'p', InputOption::VALUE_REQUIRED, 'The port to start the server'),
-                 new InputOption('base-url', 'u', InputOption::VALUE_OPTIONAL, 'The base url of the site'),
+                 new InputOption('address', 'a', InputOption::VALUE_REQUIRED, 'Address', '127.0.0.1'),
+                 new InputOption('port', 'p', InputOption::VALUE_REQUIRED, 'The port to start the server', 8900)
             ))
         ;
     }
@@ -38,6 +42,61 @@ class ServerCommand extends MeinhofCommand
     {
         if (strnatcmp(phpversion(),'5.4') < 0) {
             throw new \RuntimeException("The development server only works in php versions 5.4 and up.");
+        }
+
+        $signalHandler = function() use ($output) {
+            exit();
+        };
+
+        pcntl_signal(SIGTERM, $signalHandler);
+        pcntl_signal(SIGINT, $signalHandler);
+
+        $meinhof = $this->getMeinhof();
+        $meinhof->update();
+
+        $documentRoot = $meinhof->getParameter('filesystem.paths.web');
+
+        $phpExecutableFinder = new PhpExecutableFinder();
+
+        if (false === $binary = $phpExecutableFinder->find()) {
+            $output->writeln('<error>Unable to find PHP binary to run server</error>');
+            return;
+        }
+
+        $this->launchServer($input, $output, $documentRoot, $binary);
+
+        $this->watch($output);
+    }
+
+    protected function launchServer(InputInterface $input, OutputInterface $output, $documentRoot, $binary)
+    {
+        $output->writeln(sprintf(
+            "Server running on <info>http://%s:%s</info> with document root <info>%s</info>",
+            $input->getOption('address'),
+            $input->getOption('port'),
+            $documentRoot
+        ));
+
+        pcntl_fork();
+
+        $processBuilder = new ProcessBuilder(array(
+            $binary,
+            '-S',
+            $input->getOption('address').':' . $input->getOption('port'),
+            '--docroot',
+            $documentRoot
+        ));
+
+        $process = $processBuilder->getProcess();
+        $process->start();
+
+        if (!$process->isRunning()) {
+            $output->writeln('<error>Unable to start the server process</error>');
+            return 1;
+        }
+
+        while ($process->isRunning()) {
+            // block this thread
         }
     }
 }
--- src/Meinhof/Command/WatchCommand.php
+++ src/Meinhof/Command/WatchCommand.php
@@ -2,6 +2,7 @@
 
 namespace Meinhof\Command;
 
+use Meinhof\Helper\FileWatcherHelper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -33,5 +34,26 @@ class WatchCommand extends MeinhofCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->watch($output);
+    }
+
+    protected function watch($output)
+    {
+        $meinhof = $this->getMeinhof();
+        $meinhof->update();
+
+        $watchDir = $meinhof->getParameter('filesystem.paths.posts');
+
+        $output->writeln('Watching for changes in <info>' . $watchDir. '</info>');
+
+        $watcherHelper = new FileWatcherHelper($watchDir);
+
+        $watcherHelper->watch(function(\SplFileInfo $file) use ($meinhof, $output) {
+            $output->writeln('Dumping <info>' . $file->getFilename(). '</info>');
+
+            $meinhof->update();
+        }, function(\Exception $e) use ($output) {
+            $output->writeln('Error dumping content: <error>' . $e->getMessage(). '</error>');
+        });
     }
 }
